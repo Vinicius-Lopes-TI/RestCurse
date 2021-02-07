2c273f58e8dd8c99520765b4519ea85eb4cb0cd8
Vinícius Lopes <viniciuslopesti@hotmail.com>
2021-02-07 10:38:32 -0300

    Soma API

---
+++ 0-API/ApiCurso/ApiCurso/Controllers/CalculatorController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCurso.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController (ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        // GET api/values
+        [HttpGet("sum/{firstNumber}/{secoundNumber}")]
+        public IActionResult Get(string firstNumber, string secoundNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secoundNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secoundNumber);
+                return Ok(sum.ToString());
+            }
+
+
+            return BadRequest("Invalid Input");
+        }
+        private bool IsNumeric(string strNumber)
+        {
+            double number;
+            bool isNumber = double.TryParse(
+                strNumber,
+                System.Globalization.NumberStyles.Any,
+                System.Globalization.NumberFormatInfo.InvariantInfo,
+                out number);
+
+            return isNumber;
+        }
+
+        private decimal ConvertToDecimal(string strNumber)
+        {
+            decimal decimalValue;
+            if(decimal.TryParse(strNumber, out decimalValue))
+            {
+                return decimalValue;
+            }
+
+            return 0;
+        }
+
+       
+    }
+}
